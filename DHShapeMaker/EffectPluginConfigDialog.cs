3dd0f8fdac08c4f1bee7d957d2471b88c9fff37d
toehead2001 <toe_head2001@yahoo.com>
2016-11-29 23:35:07 -0800

    Fix for some single looped paths

--- DHShapeMaker/EffectPluginConfigDialog.cs
+++ DHShapeMaker/EffectPluginConfigDialog.cs
@@ -1237,7 +1237,7 @@ namespace ShapeMaker
                     #endregion
 
                     bool islinked = true;
-                    if (!Oldxy.Equals(pts[0]))
+                    if (!Oldxy.Equals(pts[0]) || (j == -1 && Loop.Checked))
                     {
                         loopBack = new PointF(pts[0].X, pts[0].Y);
                         islinked = false;
