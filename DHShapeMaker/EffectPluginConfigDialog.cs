aa9581dcbd73db5feae262642a10c97d512f30ff
toehead2001 <toe_head2001@yahoo.com>
2016-12-02 20:44:42 -0800

    Enable the Flip menus so Shape flips can done

--- DHShapeMaker/EffectPluginConfigDialog.cs
+++ DHShapeMaker/EffectPluginConfigDialog.cs
@@ -4085,8 +4085,8 @@ namespace ShapeMaker
             removePathToolStripMenuItem.Enabled = (LineList.SelectedIndex > -1);
             clonePathToolStripMenuItem.Enabled = (LineList.SelectedIndex > -1);
             loopPathToolStripMenuItem.Enabled = (canvasPoints.Length > 1);
-            flipHorizontalToolStripMenuItem.Enabled = (canvasPoints.Length > 1);
-            flipVerticalToolStripMenuItem.Enabled = (canvasPoints.Length > 1);
+            flipHorizontalToolStripMenuItem.Enabled = (canvasPoints.Length > 1 || LineList.Items.Count > 0);
+            flipVerticalToolStripMenuItem.Enabled = (canvasPoints.Length > 1 || LineList.Items.Count > 0);
             clearAllToolStripMenuItem.Enabled = (canvasPoints.Length > 0 || LineList.Items.Count > 0);
         }
 
