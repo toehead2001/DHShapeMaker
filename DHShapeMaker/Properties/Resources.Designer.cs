0dece32c1c210ca7a2ca564b899a324373b22e89
toehead2001 <toe_head2001@yahoo.com>
2016-12-05 12:56:13 -0800

    Remove some resources that aren't even used

--- DHShapeMaker/Properties/Resources.Designer.cs
+++ DHShapeMaker/Properties/Resources.Designer.cs
@@ -292,16 +292,6 @@ namespace ShapeMaker.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap rightarrow {
-            get {
-                object obj = ResourceManager.GetObject("rightarrow", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap rightarrowLit {
             get {
                 object obj = ResourceManager.GetObject("rightarrowLit", resourceCulture);
