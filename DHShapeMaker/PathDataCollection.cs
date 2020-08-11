793668314815fb8421a666a91e42ba4235864f73
toehead2001 <toehead2001@users.noreply.github.com>
2020-08-10 22:15:59 -0600

    Remove usings

--- DHShapeMaker/PathDataCollection.cs
+++ DHShapeMaker/PathDataCollection.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
-using PaintDotNet.Collections;
 
 namespace ShapeMaker
 {
